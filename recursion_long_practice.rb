40650a5dcde0533ca6035bef3b9dbf610e99616b
ChristopherJtrent <ChristopherJtrent@outlook.com>
2024-01-24 11:30:49 -0800

    implement recursive fibonacci method

--- recursion_long_practice.rb
+++ recursion_long_practice.rb
@@ -23,7 +23,7 @@ def exp_v1(base, exp)
     base * exp_v1(base, exp-1) 
 end
 
-puts exp_v1(2, 6)
+#puts exp_v1(2, 6)
 
 def exp_v2(base, exp)
     return 1 if exp == 0
@@ -36,4 +36,52 @@ def exp_v2(base, exp)
     return base * (result*result)
 end
 
-puts exp_v2(2, 6)
+#puts exp_v2(2, 6)
+
+# string* mechanical = ["nuts", "bolts" , "washers"]
+# string* electrical = ["capacitors", "resistors", "inductors"]
+# robot parts = [mechanical, electrical]
+# robot_parts_copy = robot_parts.shallow_copy
+# robot_parts_copy[1] << "LEDs"
+# electrical << "LEDs"
+
+class Array 
+    def deep_dup
+        result = []
+        self.each do |ele|
+            if ele.is_a?(Array)
+                result << ele.deep_dup
+            else
+                result << ele
+            end
+        end
+        result
+    end
+end
+
+# robot_parts = [
+#   ["nuts", "bolts", "washers"],
+#   ["capacitors", "resistors", "inductors"]
+# ]
+
+# robot_parts.each {|e| puts e.object_id}
+# copy = robot_parts.deep_dup
+# copy.each {|e| puts e.object_id}
+
+# recursive
+def fibonacci_v1(n)
+    return [] if n <= 0
+    if n == 1 
+        return [1]
+    elsif n == 2
+        return [1,1]
+    else 
+        temp = fibonacci_v1(n-1)
+        # longer, slower way to write 83 (O(n^3))
+        # fibonacci_v1(n-1) << fibonacci_v1(n-1)[-1] + fibonacci_v1(n-1)[-2]
+        # shorter, faster way to write, (O(n))
+        temp << (temp[-1] + temp[-2])
+    end
+end
+
+p fibonacci_v1(30)
