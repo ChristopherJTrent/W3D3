f7c937cbaab775a131f3d4862f35274ea4c72656
Caroline Zhang <cczhang1@yahoo.com>
2024-01-24 10:47:05 -0800

    Updates with exp_v1 and exp_v2 methods

--- recursion_long_practice.rb
+++ recursion_long_practice.rb
@@ -16,4 +16,24 @@ def range(start, end_num)
     #   [1,2,3,4,5]
 end
 
-print range(1,5)
+# puts range(1,5)
+
+def exp_v1(base, exp)
+    return 1 if exp == 0
+    base * exp_v1(base, exp-1) 
+end
+
+puts exp_v1(2, 6)
+
+def exp_v2(base, exp)
+    return 1 if exp == 0
+    return base if exp == 1
+    if exp.even?
+        result = exp_v2(base, exp/2)
+        return result * result
+    end
+    result = exp_v2(base, (exp-1)/2)
+    return base * (result*result)
+end
+
+puts exp_v2(2, 6)
